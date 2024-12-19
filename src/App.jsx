4fbeec9f0f24978bf3952f82f90f9ba1e0be2dc1
harshalkale0911 <hkale6888@gmail.com>
2024-12-19 12:17:33 +0530

    fhfh

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,25 @@
-import './App.css';
+import "./App.css";
 import elephant from "./images/elephant.jpeg";
 
-const imageData = ()=>{
-  let data = [
-    {
-      id:1,
-      img:elephant
-    },
-    {
-      id:2,
-      img:elephant
-    },
-    {
-      id:3,
-      img:elephant
-    },
-    {
-      id:4,
-      img:elephant
-    }
-  ]
-  return data;
-}
+const imageData = () => {
+  return [
+    { id: 1, img: elephant },
+    { id: 2, img: elephant },
+    { id: 3, img: elephant },
+    { id: 4, img: elephant },
+  ];
+};
 
 function App() {
-  // code here
+  const images = imageData(); // Get image data
+
+  return (
+    <div className="container">
+      {images.map((item) => (
+        <img key={item.id} src={item.img} alt={`Elephant ${item.id}`} className="image" />
+      ))}
+    </div>
+  );
 }
 
 export default App;
--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -1,30 +1,26 @@
 import { Component } from "react";
-import "./App.css"
+import "./App.css";
 import elephant from "./images/elephant.jpeg";
 
-export default class AppClass extends Component{
-  
-  imageData = ()=>{
-    let data = [
-      {
-        id:1,
-        img:elephant
-      },
-      {
-        id:2,
-        img:elephant
-      },
-      {
-        id:3,
-        img:elephant
-      },
-      {
-        id:4,
-        img:elephant
-      }
-    ]
-    return data;
-  }
+export default class AppClass extends Component {
+  imageData = () => {
+    return [
+      { id: 1, img: elephant },
+      { id: 2, img: elephant },
+      { id: 3, img: elephant },
+      { id: 4, img: elephant },
+    ];
+  };
 
-  // code here
+  render() {
+    const images = this.imageData(); // Get image data
+
+    return (
+      <div className="container">
+        {images.map((item) => (
+          <img key={item.id} src={item.img} alt={`Elephant ${item.id}`} className="image" />
+        ))}
+      </div>
+    );
+  }
 }
